629fc6cafa56769c79c52522803bdf4ff9539cb2
David Parrish <daveparrish@gmail.com>
2014-12-10 16:58:24 -0500

    Fetch from 'https'

--- fetch_soundcloud_info.js
+++ fetch_soundcloud_info.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     url = $("input[name='url']").val();
     id = $("input[name='id']").val();
  
-    $.get('http://api.soundcloud.com/resolve.json?url='+
+    $.get('https://api.soundcloud.com/resolve.json?url='+
       url+'/tracks&client_id='+id, function (result) {
       $('#output').text(JSON.stringify(result, undefined, 2))
     });
